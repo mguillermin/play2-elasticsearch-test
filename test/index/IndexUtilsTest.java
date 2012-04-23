3403e771d1c5dfad4850eb6ec273cb7dad8a5e96
Matthieu Guillermin <matthieu.guillermin@gmail.com>
2012-04-23 22:27:36 +0200

    IndexUtils testing update

--- test/index/IndexUtilsTest.java
+++ test/index/IndexUtilsTest.java
@@ -2,22 +2,36 @@ package index;
 
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.junit.Test;
+import org.junit.*;
+
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
 import java.io.IOException;
 
 /**
- * User: mguillermin
- * Date: 23/04/12
+ * IndexUtils class tests
  */
 public class IndexUtilsTest {
 
-  @Test
-  public void riverListTest() throws IOException {
+  @BeforeClass
+  public static void before() {
     IndexUtils.initClient(true);
+  }
 
+  @AfterClass
+  public static void after() {
+    IndexUtils.closeClient();
+  }
+
+  @Test
+  public void initIndex() {
+    assertThat(IndexUtils.get()).isNotNull();
+    assertThat(IndexUtils.get().client).isNotNull();
+  }
+
+  @Test
+  public void simpleIndexation() throws IOException {
     IndexUtils.get().clearIndex(Document.INDEX_NAME);
 
     Document document = Document.randomDocument();
@@ -29,6 +43,15 @@ public class IndexUtilsTest {
         .execute()
         .actionGet();
     assertThat(searchResponse.getHits().totalHits()).isEqualTo(1);
+  }
+
+  @Test
+  public void deleteDocument() throws IOException {
+    IndexUtils.get().clearIndex(Document.INDEX_NAME);
+
+    Document document = Document.randomDocument();
+    String docId = IndexUtils.get().index(document);
+    IndexUtils.get().refresh();
 
     IndexUtils.get().delete(Document.INDEX_NAME, Document.TYPE, docId);
     IndexUtils.get().refresh();
@@ -38,6 +61,5 @@ public class IndexUtilsTest {
         .execute()
         .actionGet();
     assertThat(searchResponse2.getHits().totalHits()).isEqualTo(0);
-
   }
 }
